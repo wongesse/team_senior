abb0a292c283080ad96bd85ea15d9231d09c1662
Han Hong <han.hong.ky@gmail.com>
2018-10-02 12:02:36 -0500

    Main Created

---
+++ Main/mobile/src/main/java/com/mobvoi/ticwear/mobvoiapidemo/SemanticActivity.java
@@ -0,0 +1,14 @@
+package com.mobvoi.ticwear.mobvoiapidemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SemanticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semantic);
+    }
+
+}
