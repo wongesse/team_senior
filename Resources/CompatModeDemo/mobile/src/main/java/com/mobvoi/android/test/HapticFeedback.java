03c0e5d0f94ce2982866d07528c89641d7cfd7e3
wongesse <nickwong@sbcglobal.net>
2018-11-27 17:25:41 -0600

    pushing haptic feeback stuff

---
+++ Resources/CompatModeDemo/mobile/src/main/java/com/mobvoi/android/test/HapticFeedback.java
@@ -0,0 +1,46 @@
+package com.mobvoi.android.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HapticFeedback extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_screen);
+
+        final Button haptic_button = findViewById(R.id.haptic);
+        haptic_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        final Button response_button = findViewById(R.id.response);
+        response_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        final Button state_button = findViewById(R.id.state);
+        state_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        final Button return_button = findViewById(R.id.return_button);
+        return_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+    }
+
+}
