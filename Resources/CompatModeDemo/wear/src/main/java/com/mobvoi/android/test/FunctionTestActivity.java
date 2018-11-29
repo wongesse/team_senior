d0104655ef8873a19ca8e579449345ac45c3fcbd
wongesse <nickwong@sbcglobal.net>
2018-11-29 11:16:44 -0600

    vibrator

--- Resources/CompatModeDemo/wear/src/main/java/com/mobvoi/android/test/FunctionTestActivity.java
+++ Resources/CompatModeDemo/wear/src/main/java/com/mobvoi/android/test/FunctionTestActivity.java
@@ -173,7 +173,7 @@ public class FunctionTestActivity extends Activity implements SensorEventListene
 
             //
             Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-            long[] vibrationPattern = {0, 500, 50, 300};
+            long[] vibrationPattern = {0, 500, 50, 500, 50, 500, 50, 500};
             //-1 - don't repeat
             final int indexInPatternToRepeat = -1;
             vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
